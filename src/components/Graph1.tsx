e60945bc3641fbde5afb58c6403e50fe6739e1cf
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-03-19 16:22:16 +0900

    ここのコンポーネントを修正

--- src/components/Graph1.tsx
+++ src/components/Graph1.tsx
@@ -1,74 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { ComposedChart, Bar, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { Button } from '@material-ui/core';
+import * as Types from '../types/main';
+import { getEachData } from '../API/main';
 
 const Graph1Container = styled.div`
   display: inline-block;
   min-height: calc(100vh - 100px);
   width: calc((100% - 10px) / 2);
+  text-align: center;
 `;
 
-const data = [
-  {
-    device_id: 'XX0921', // string
-    points: 28308, // num
-    date: '2000-01-01T01:00:00+09:00',
-  },
-  {
-    device_id: 'XX0832',
-    points: 5839,
-    date: '2000-01-01T01:00:00+09:00',
-  },
-  {
-    device_id: 'XX0832',
-    points: 11123,
-    date: '2000-01-01T01:00:00+09:00',
-  },
-];
+interface Props {
+  data: Array<Types.Graph2> | undefined;
+}
 
-const Graph1: React.FC = () => {
-  const newData: {
-    device_id: string;
-    points: number;
-    date: string;
-    name: string;
-  }[] = [];
-
-  for (const tmp of data) {
-    newData.push({
-      device_id: tmp.device_id,
-      points: tmp.points,
-      date: new Date(tmp.date).toDateString(),
-      name: tmp.device_id + '   ' + new Date(tmp.date).toDateString(),
-    });
-  }
-
-  if (newData) {
-    return (
-      <Graph1Container>
-        <h1>
-          <span style={{ fontSize: 15 }}>各測定ごとのランキング</span>
-        </h1>
-        <ComposedChart
-          layout="vertical"
-          data={newData}
-          width={540}
-          height={400}
-          margin={{
-            top: 20,
-            left: 50,
-          }}
-          style={{ paddingLeft: 30 }}
-        >
-          <CartesianGrid stroke="#f1eae3" />
-          <XAxis type="number" fontSize={10} />
-          <YAxis dataKey="name" type="category" fontSize={10} />
-          <Bar dataKey="points" barSize={18} fill="gray" />
-        </ComposedChart>
-      </Graph1Container>
-    );
-  }
-  return <div />;
+const Graph1: React.FC<Props> = (props: Props) => {
+  return (
+    <Graph1Container>
+      <h1>
+        <span style={{ fontSize: 15 }}>1回ごとのランキング</span>
+      </h1>
+      <ComposedChart
+        layout="vertical"
+        data={props.data}
+        width={540}
+        height={400}
+        margin={{
+          left: 50,
+        }}
+        style={{ paddingLeft: 30 }}
+      >
+        <CartesianGrid stroke="#f1eae3" strokeDasharray="3 3" />
+        <XAxis type="number" fontSize={10} />
+        <YAxis dataKey="device_id" type="category" fontSize={10} />
+        <Bar dataKey="points" barSize={18} fill="gray" />
+      </ComposedChart>
+      <div
+        style={{ float: 'right', paddingRight: '100px', paddingTop: '20px' }}
+      >
+        <Button variant="outlined">詳しく見る</Button>
+      </div>
+    </Graph1Container>
+  );
 };
 
 export default Graph1;
--- src/components/Graph2.tsx
+++ src/components/Graph2.tsx
@@ -1,32 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { ComposedChart, Bar, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { Button } from '@material-ui/core';
+import * as Types from '../types/main';
 
 const Graph2Container = styled.div`
   display: inline-block;
   min-height: calc(100vh - 100px);
   width: calc((100% - 10px) / 2);
+  text-align: center;
 `;
 
-const data = [
-  {
-    device_id: 'XX0921', // string
-    points: 283082, // num
-    date: '2000-01-01T01:00:00+09:00',
-  },
-  {
-    device_id: 'XX0832',
-    points: 5839,
-    date: '2000-01-01T01:00:00+09:00',
-  },
-  {
-    device_id: 'XX0813',
-    points: 11123,
-    date: '2000-01-01T01:00:00+09:00',
-  },
-];
+interface Props {
+  data: Array<Types.Graph2> | undefined;
+}
 
-const Graph2: React.FC = () => {
+const Graph2: React.FC<Props> = (props: Props) => {
   return (
     <Graph2Container>
       <h1>
@@ -34,21 +23,24 @@ const Graph2: React.FC = () => {
       </h1>
       <ComposedChart
         layout="vertical"
-        data={data}
+        data={props.data}
         width={540}
         height={400}
         margin={{
-          top: 20,
           left: 50,
         }}
         style={{ paddingLeft: 30 }}
       >
-        <CartesianGrid stroke="#f1eae3" />
-
+        <CartesianGrid stroke="#f1eae3" strokeDasharray="3 3" />
         <XAxis type="number" fontSize={12} />
         <YAxis dataKey="device_id" type="category" fontSize={10} />
         <Bar dataKey="points" barSize={18} fill="gray" />
       </ComposedChart>
+      <div
+        style={{ float: 'right', paddingRight: '100px', paddingTop: '20px' }}
+      >
+        <Button variant="outlined">詳しく見る</Button>
+      </div>
     </Graph2Container>
   );
 };
