c025e6f93dc1f7ec039e583cb761fdbe2093d2b5
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-03-19 16:54:39 +0900

    ルーティング テンプレート作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import EachContainer from './components/Each';
+import TotalContainer from './components/Total';
 
 function App() {
   return (
     <div className="App">
       <div className="App-container">
         <Header />
-        <Main />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/data/each" component={EachContainer} />
+            <Route exact path="/data/total" component={TotalContainer} />
+          </Switch>
+        </Router>
       </div>
     </div>
   );
