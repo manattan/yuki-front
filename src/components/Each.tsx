d67529346c04889c36700eb84e9f601de6a48bb9
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-03-19 20:40:34 +0900

    詳細画面

--- src/components/Each.tsx
+++ src/components/Each.tsx
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { ComposedChart, Bar, XAxis, YAxis, CartesianGrid } from 'recharts';
+import {
+  ComposedChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts';
+import { CircularProgress } from '@material-ui/core';
 import * as Types from '../types/main';
 import { getEachData } from '../API/main';
 
 const Container = styled.div`
   margin: 40px;
   min-height: calc(100vh - 160px);
-  border: 2px solid red;
   padding: 0 0 0 20px;
 `;
 
@@ -21,24 +28,43 @@ const EachContainer: React.FC = () => {
     }, 1000);
   }, []);
 
+  if (data) {
+    return (
+      <Container>
+        <p>個別の結果ランキング</p>
+        <ComposedChart
+          layout="vertical"
+          data={data}
+          width={1200}
+          height={500}
+          margin={{
+            left: 50,
+          }}
+          style={{ paddingLeft: 30 }}
+        >
+          <CartesianGrid stroke="#f1eae3" strokeDasharray="3 3" />
+          <Tooltip
+            wrapperStyle={{
+              width: '120px',
+              fontSize: '30%',
+              textAlign: 'center',
+            }}
+          />
+          <XAxis type="number" fontSize={10} />
+          <YAxis dataKey="device_id" type="category" fontSize={10} />
+          <Bar dataKey="points" barSize={18} fill="gray" />
+        </ComposedChart>
+      </Container>
+    );
+  }
+
   return (
-    <Container>
-      <p>個別の結果ランキング</p>
-      <ComposedChart
-        layout="vertical"
-        data={data}
-        width={1200}
-        height={500}
-        margin={{
-          left: 50,
-        }}
-        style={{ paddingLeft: 30 }}
-      >
-        <CartesianGrid stroke="#f1eae3" strokeDasharray="3 3" />
-        <XAxis type="number" fontSize={10} />
-        <YAxis dataKey="device_id" type="category" fontSize={10} />
-        <Bar dataKey="points" barSize={18} fill="gray" />
-      </ComposedChart>
+    <Container style={{ textAlign: 'center' }}>
+      <CircularProgress
+        color="inherit"
+        size={80}
+        style={{ marginTop: '100px' }}
+      />
     </Container>
   );
 };
--- src/components/Total.tsx
+++ src/components/Total.tsx
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import {
+  ComposedChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts';
+import { CircularProgress } from '@material-ui/core';
+import * as Types from '../types/main';
+import { getTotalData } from '../API/main';
+
+const Container = styled.div`
+  margin: 40px;
+  min-height: calc(100vh - 160px);
+  padding: 0 0 0 20px;
+`;
 
 const TotalContainer: React.FC = () => {
-  return <div />;
+  const [data, setData] = useState<Array<Types.Graph2> | undefined>();
+
+  useEffect(() => {
+    setInterval(async () => {
+      const results = await getTotalData();
+      setData(await results.json());
+    }, 1000);
+  }, []);
+
+  if (data) {
+    return (
+      <Container>
+        <p>累積得点ランキング</p>
+        <ComposedChart
+          layout="vertical"
+          data={data}
+          width={1200}
+          height={500}
+          margin={{
+            left: 50,
+          }}
+          style={{ paddingLeft: 30 }}
+        >
+          <CartesianGrid stroke="#f1eae3" strokeDasharray="3 3" />
+          <Tooltip
+            wrapperStyle={{
+              width: '120px',
+              fontSize: '30%',
+              textAlign: 'center',
+            }}
+          />
+          <XAxis type="number" fontSize={10} />
+          <YAxis dataKey="device_id" type="category" fontSize={10} />
+          <Bar dataKey="points" barSize={18} fill="gray" />
+        </ComposedChart>
+      </Container>
+    );
+  }
+
+  return (
+    <Container style={{ textAlign: 'center' }}>
+      <CircularProgress
+        color="inherit"
+        size={80}
+        style={{ marginTop: '100px' }}
+      />
+    </Container>
+  );
 };
 
 export default TotalContainer;
