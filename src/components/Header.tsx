60f22aee2c621d6ff531e05b0d65cee39705951e
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-03-19 16:20:53 +0900

    headerの修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Button } from '@material-ui/core';
 
 const HeadContainer = styled.div`
   height: 60px;
@@ -9,8 +10,24 @@ const HeadContainer = styled.div`
 const Header: React.FC = () => {
   return (
     <HeadContainer>
-      <div style={{ padding: '10px 0', color: 'white', fontWeight: 'bold' }}>
+      <div
+        style={{
+          padding: '10px 100px',
+          color: 'white',
+          fontWeight: 'bold',
+          display: 'inline-block',
+        }}
+      >
         雪かきpoint計測するくん
+      </div>
+      <div
+        style={{
+          display: 'inline-block',
+          float: 'right',
+          padding: '10px 30px',
+        }}
+      >
+        <Button>ログイン</Button>
       </div>
     </HeadContainer>
   );
